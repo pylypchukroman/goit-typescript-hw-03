9e2a5b3704de880b066a2b6ebd1fd84d4a609d80
pylypchukroman <romanpylypchuk@gmail.com>
2023-11-16 16:48:11 +0200

    WIP

--- src/2.ts
+++ src/2.ts
@@ -16,11 +16,7 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
+  constructor(public name: string, private department: string, protected salary: number) {
     this.name = name;
     this.department = department;
     this.salary = salary;
