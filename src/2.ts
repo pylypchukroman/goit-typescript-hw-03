27d2f4646c7ba3c2b28d6aaef058e1156043f0be
pylypchukroman <romanpylypchuk@gmail.com>
2023-11-16 19:46:56 +0200

    WIP: added solution for task 2

--- src/2.ts
+++ src/2.ts
@@ -29,6 +29,9 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 1000);
+  }
 }
 
 
